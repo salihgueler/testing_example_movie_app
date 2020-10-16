0daa0b207ea8e1652787a0de65dd3b905a61fc2f
salihgueler <muhammedsalihguler@gmail.com>
2020-10-16 11:18:06 +0200

    Adding tests.

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class _MovieAppState extends State<MovieApp> {
                   title: Text('Movie App'),
                 ),
                 floatingActionButton: FloatingActionButton.extended(
+                  key: Key('remaining-money-indicator'),
                   onPressed: null,
                   label: Text(
                     'Remaining Money: ${snapshot.data}',
@@ -38,41 +39,8 @@ class _MovieAppState extends State<MovieApp> {
                     AsyncSnapshot<List<Movie>> snapshot,
                   ) {
                     if (snapshot.hasData) {
-                      return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder:
-                            (BuildContext builderContext, int position) =>
-                                ListTile(
-                          title: Text(snapshot.data[position].title),
-                          subtitle: Text(
-                            'Price: ${snapshot.data[position].price.toString()}',
-                          ),
-                          onTap: () async {
-                            final movie = snapshot.data[position];
-                            final purchaseResult =
-                                _movieRepository.buyTicket(movie);
-
-                            if (!purchaseResult.result) {
-                              showDialog(
-                                context: builderContext,
-                                builder: (_) => AlertDialog(
-                                  title: const Text('Sorry!'),
-                                  content: Text(
-                                    'Your last purchase for ${movie.title}\nwas not successful.\n\nYour remaining cash is ${purchaseResult.remainingMoney}.\n\nPlease either try again after topping up your account or buy a movie in your price range.',
-                                  ),
-                                  actions: <Widget>[
-                                    FlatButton(
-                                      child: Text('Close me!'),
-                                      onPressed: () =>
-                                          Navigator.of(builderContext).pop(),
-                                    )
-                                  ],
-                                ),
-                              );
-                            }
-                          },
-                        ),
-                      );
+                      return MovieList(snapshot.data,
+                          (Movie movie) => _movieRepository.buyTicket(movie));
                     } else {
                       return Center(
                         child: CircularProgressIndicator(),
@@ -94,5 +62,48 @@ class _MovieAppState extends State<MovieApp> {
   void dispose() {
     _movieRepository.close();
     super.dispose();
+  }
+}
+
+class MovieList extends StatelessWidget {
+  final List<Movie> movies;
+  final Function(Movie) buyMovieAction;
+  const MovieList(this.movies, this.buyMovieAction);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      key: Key('movie-list'),
+      itemCount: movies.length,
+      itemBuilder: (BuildContext builderContext, int position) => ListTile(
+        key: Key(movies[position].title),
+        title: Text(movies[position].title),
+        subtitle: Text(
+          'Price: ${movies[position].price.toString()}',
+        ),
+        onTap: () async {
+          final movie = movies[position];
+          final purchaseResult = buyMovieAction.call(movie);
+
+          if (!purchaseResult.result) {
+            showDialog(
+              context: builderContext,
+              builder: (_) => AlertDialog(
+                key: Key('info-dialog'),
+                title: const Text('Sorry!'),
+                content: Text(
+                  'Your last purchase for ${movie.title}\nwas not successful.\n\nYour remaining cash is ${purchaseResult.remainingMoney}.\n\nPlease either try again after topping up your account or buy a movie in your price range.',
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Close me!'),
+                    onPressed: () => Navigator.of(builderContext).pop(),
+                  )
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
