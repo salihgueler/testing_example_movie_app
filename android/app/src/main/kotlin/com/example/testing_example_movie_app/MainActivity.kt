e0e2fff7cc8a594b9effc6e67c2acd7ddb0aa1fb
salihgueler <muhammedsalihguler@gmail.com>
2020-10-16 11:28:08 +0200

    Add the .gitignore

--- android/app/src/main/kotlin/com/example/testing_example_movie_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testing_example_movie_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test_driver/app.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:testing_example_movie_app/main.dart' as app;
-
-void main() {
-  enableFlutterDriverExtension();
-  app.main();
-}
