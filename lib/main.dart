d7b1ef8b009d997e3ca1c33713d9f084ee780cd5
salihgueler <muhammedsalihguler@gmail.com>
2020-10-16 23:14:35 +0200

    Navigator API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:testing_example_movie_app/movie/data/movie.dart';
-import 'package:testing_example_movie_app/movie/data/movie_repository.dart';
 
 void main() {
   runApp(MovieApp());
@@ -12,97 +11,142 @@ class MovieApp extends StatefulWidget {
 }
 
 class _MovieAppState extends State<MovieApp> {
-  final MovieRepository _movieRepository = MovieRepository();
-
+  final movies = const <Movie>[
+    Movie(
+      title: 'Palm Springs',
+      price: 16,
+    ),
+    Movie(
+      title: 'Jojo Rabbit',
+      price: 20,
+    ),
+    Movie(
+      title: 'Waves',
+      price: 14,
+    ),
+    Movie(
+      title: 'The Last Black Man in San Francisco',
+      price: 18,
+    ),
+    Movie(
+      title: 'Hamilton',
+      price: 32,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StreamBuilder<double>(
-        stream: _movieRepository.currentValue,
-        builder: (_, AsyncSnapshot<double> snapshot) {
-          if (snapshot.hasData) {
-            return Scaffold(
-                appBar: AppBar(
-                  title: Text('Movie App'),
-                ),
-                floatingActionButton: FloatingActionButton.extended(
-                  key: Key('remaining-money-indicator'),
-                  onPressed: null,
-                  label: Text(
-                    'Remaining Money: ${snapshot.data}',
+      routes: {
+        '/empty-page': (BuildContext context) => MovieEmptyDetail(),
+        '/with-sent-data': (BuildContext context) => MovieDetailWithArguments()
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Movies'),
+        ),
+        body: ListView.builder(
+          key: Key('movie-list'),
+          itemCount: movies.length,
+          itemBuilder: (BuildContext builderContext, int position) => ListTile(
+            key: Key(movies[position].title),
+            title: Text(movies[position].title),
+            subtitle: Text(
+              'Price: ${movies[position].price.toString()}',
+            ),
+            onTap: () {
+              if (position.isEven) {
+                Navigator.push(
+                  builderContext,
+                  MaterialPageRoute(
+                    builder: (_) => MovieDetailInjectedData(
+                      movies[position],
+                    ),
                   ),
-                ),
-                body: FutureBuilder<List<Movie>>(
-                  future: _movieRepository.fetchMovies(),
-                  builder: (
-                    _,
-                    AsyncSnapshot<List<Movie>> snapshot,
-                  ) {
-                    if (snapshot.hasData) {
-                      return MovieList(snapshot.data,
-                          (Movie movie) => _movieRepository.buyTicket(movie));
-                    } else {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
-                ));
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+                );
+              } else {
+                Navigator.pushNamed(
+                  builderContext,
+                  '/with-sent-data',
+                  arguments: movies[position],
+                );
+              }
+            },
+          ),
+        ),
       ),
     );
   }
+}
 
+class MovieEmptyDetail extends StatelessWidget {
   @override
-  void dispose() {
-    _movieRepository.close();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Empty Detail Page',
+        ),
+      ),
+    );
   }
 }
 
-class MovieList extends StatelessWidget {
-  final List<Movie> movies;
-  final Function(Movie) buyMovieAction;
-  const MovieList(this.movies, this.buyMovieAction);
+class MovieDetailInjectedData extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailInjectedData(this.movie);
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      key: Key('movie-list'),
-      itemCount: movies.length,
-      itemBuilder: (BuildContext builderContext, int position) => ListTile(
-        key: Key(movies[position].title),
-        title: Text(movies[position].title),
-        subtitle: Text(
-          'Price: ${movies[position].price.toString()}',
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Injected Data Page',
         ),
-        onTap: () async {
-          final movie = movies[position];
-          final purchaseResult = buyMovieAction.call(movie);
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              movie.title,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            Text(
+              movie.price.toString(),
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          if (!purchaseResult.result) {
-            showDialog(
-              context: builderContext,
-              builder: (_) => AlertDialog(
-                key: Key('info-dialog'),
-                title: const Text('Sorry!'),
-                content: Text(
-                  'Your last purchase for ${movie.title}\nwas not successful.\n\nYour remaining cash is ${purchaseResult.remainingMoney}.\n\nPlease either try again after topping up your account or buy a movie in your price range.',
-                ),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text('Close me!'),
-                    onPressed: () => Navigator.of(builderContext).pop(),
-                  )
-                ],
-              ),
-            );
-          }
-        },
+class MovieDetailWithArguments extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final movie = ModalRoute.of(context).settings.arguments as Movie;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Detail Page',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              movie.title,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            Text(
+              movie.price.toString(),
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ],
+        ),
       ),
     );
   }
